2b14e1f93d8fa8182ac72c0b21831a5ec07f8fd4
Tyler Ballard <tyler.ballard1@outlook.com>
2019-03-26 12:11:18 -0700

    Add files via upload
    Optimized version

--- CalcXOfTwoLines/Line.cs
+++ CalcXOfTwoLines/Line.cs
@@ -8,10 +8,8 @@ namespace CalculateXOfTwoLines
 {
     class Line
     {
-        public double ASlope { get; set; }
-        public double AYInt { get; set; }
-        public double BSlope { get; set; }
-        public double BYInt { get; set; }
+        public double Slope { get; set; }
+        public double YInt { get; set; }
     }
     class FindX
     {
@@ -22,23 +20,29 @@ namespace CalculateXOfTwoLines
             Line lineB = new Line();
 
             Console.Write("Enter the Slope of line A: ");
-            lineA.ASlope = double.Parse(Console.ReadLine());
+            lineA.Slope = double.Parse(Console.ReadLine());
             Console.Write("Enter the Y-Intercept of line A: ");
-            lineA.AYInt = double.Parse(Console.ReadLine());
+            lineA.YInt = double.Parse(Console.ReadLine());
             Console.Write("Enter the Slope of line B: ");
-            lineB.BSlope = double.Parse(Console.ReadLine());
+            lineB.Slope = double.Parse(Console.ReadLine());
             Console.Write("Enter the Y-Intercept of line B: ");
-            lineB.BYInt = double.Parse(Console.ReadLine());
+            lineB.YInt = double.Parse(Console.ReadLine());
 
-            if (lineA.ASlope == lineB.BSlope && lineA.AYInt != lineB.BYInt)
+            if (lineA.Slope == lineB.Slope)
             {
                 Console.Write("You are trying to determine the value of the x-intercept of the point where the lines intersect for two parallel lines. There is no point of intersect, so x cannot be determined: ");
                 x = 0;
                 return x;
             }
+            if (lineA.Slope == lineB.Slope && lineA.YInt == lineB.YInt)
+            {
+                Console.Write("You are trying to determine the value of the x-intercept of the point where the lines intersect for two of the same lines. There is no point of intersect, so x cannot be determined: ");
+                x = 0;
+                return x;
+            }
             else
             {
-                x = (lineA.ASlope - lineB.BSlope) / (lineA.AYInt - lineB.BYInt);
+                x = (lineA.Slope - lineB.Slope) / (lineA.YInt - lineB.YInt);
                 Console.Write("X equals: ");
                 return x;
             }        
